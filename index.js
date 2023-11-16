ac1ee96c9622eee5e28fa0d28f01770abb870c81
Isabella Vieira <110853924+ivieira1@users.noreply.github.com>
2023-11-16 15:24:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-let NivelJogador = 1
-if (NivelJogador) > 1000; "Ferro"
-console.log (NivelJogador)
+let NivelJogador = ""
+let NomeJogador = "Estevão"
+let PontosJogador = 5000
+
+//Estruturas Condicionais//
+if (PontosJogador < 1000)
+{ NivelJogador = "Ferro"
+}
+else if (NivelJogador > 1001 && NivelJogador < 2000) {
+    NivelJogador = "Bronze"}
+else if (NivelJogador > 2001 && NivelJogador < 5000) {
+    NivelJogador = "Prata"
+}
+else if (PontosJogador > 5001 && PontosJogador < 7000) {
+    NivelJogador = "Ouro"
+}
+else if (PontosJogador > 7001 && PontosJogador < 8000) {
+    NivelJogador = "Platina"
+}
+else if (PontosJogador > 8001 && PontosJogador < 9000){
+    NivelJogador = "Ascendente"
+}
+else if (PontosJogador > 9001 && PontosJogador < 10000) {
+    NivelJogador = "Imortal"
+}
+else if (PontosJogador === 10001 || PontosJogador > 10001) {
+    NivelJogador = "Radiante"
+}
+// Mensagem de Saída//
+console.log("O Herói de nome " + NomeJogador+ "está no nível de" + NivelJogador)
